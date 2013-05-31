2a164d0b575cdc6d45c262eef020b8476515133d
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-31 13:26:57 -0500

    create tasks migration

--- db/migrate/20130531131100_create_tasks_table.rb
+++ db/migrate/20130531131100_create_tasks_table.rb
@@ -1,1 +1,12 @@
-fdjksalh
+
+class CreateTasksTable < ActiveRecord::Migration
+  def change
+    create_table(:tasks) do |t|
+      t.string  :name
+      t.string  :description
+      t.integer :list_id
+
+      t.timestamps
+    end
+  end
+end 
